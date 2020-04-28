673237da4b3bb5bf335412b26c39859e4c5f5694
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-04-28 16:31:06 -0500

    Added variables to the script

---
+++ script.js
@@ -0,0 +1,15 @@
+// variables
+var score = 0;
+var timeLeft = 30;
+var questionArray = [
+    //[Question, Array of Answers, Index of Correct Answer]
+    ['What is the answer?',[],0]
+    ['What is the answer?',[],0]
+    ['What is the answer?',[],0]
+]
+var highScoreRecord = localStorage.getItem("highScores");
+
+// Function for View High Score Button Change Page
+
+
+// Function For moving from Start Page to Questions
