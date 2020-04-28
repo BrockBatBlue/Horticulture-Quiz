06a1b17591560d524888fb905856f44fe05d7d99
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-04-28 16:51:17 -0500

    Added function to viewHighScores button to change the page section and remove all previous sections along with removing the button

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ var questionArray = [
 var highScoreRecord = localStorage.getItem("highScores");
 
 // Function for View High Score Button Change Page
+var viewHighScorePage = function(){
+
+    // Select All Sections
+    var sectionArray = document.querySelectorAll("section");
+    // Turn Off All Sections
+    for(i=0; i<sectionArray.length; i++) {
+        sectionArray[i].style.setProperty("display","none","important");
+    }
+    // Select HighScore Section
+    var highScoreSection = document.getElementById("highScores");
+    
+    // Turn On HighScore Section
+    highScoreSection.style.display = null;
+    // Hide View High Score Button
+    var highScoreButton = document.getElementById("viewHighScore");
+    highScoreButton.style.setProperty("display","none");
+}
+
 
 
 // Function For moving from Start Page to Questions
